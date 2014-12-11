5fb1ee75463dde1b249a9d55df12769a60a9e56d
findobj <findobj@hotmail.com>
2014-12-11 16:46:47 +0800

    commit

--- jni/findobj/component/AppDelegate.cpp
+++ jni/findobj/component/AppDelegate.cpp
@@ -12,10 +12,13 @@ AppDelegate::~AppDelegate()
 
 int AppDelegate::main()
 {
+	Log::i("AppDelegate", "main start");
+
 	AStar *astar = new AStar();
 	astar->test();
 	delete astar;
 
+	Log::i("AppDelegate", "main end");
 	return 0;
 }
 
